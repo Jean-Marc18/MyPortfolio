daee72519cc6c8c355d2f2b8a81ab301de5503e0
Jean-Marc18 <pich@gmail.com>
2023-05-31 21:11:29 +0000

    footer added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
+/* eslint-disable no-unused-vars */
 import React from 'react'
-import { useEffect, useState } from "react"
+import { useState } from "react"
 import { Link } from 'react-router-dom'
 
 import { styles } from '../styles';
